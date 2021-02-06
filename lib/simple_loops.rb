1e9c4c14c022a53cbe727e03fd676ee300dae3e4
Jose Mendoza <pball626@gmail.com>
2021-02-06 23:41:19 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,8 +1,8 @@
 
-while loop_message_five_times do(message)
+def loop_message_five_times(message)
   count = 0
   while count < (5) do
-  message = ["Hi!"]
+  puts message
     count += 1
   end
 end

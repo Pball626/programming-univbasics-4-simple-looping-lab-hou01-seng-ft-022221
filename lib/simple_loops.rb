522816f31e3b99067cf48cf2f8dbabb22a17f1a2
Jose Mendoza <pball626@gmail.com>
2021-02-07 00:00:51 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -15,10 +15,4 @@ def loop_message_n_times(message, n)
   end
 end
 
-def output_array(array)
-  count = 0 
-  while count < array.length do 
-    puts array[count]
-    count += 1 
-  end
-end
+def return_string_array(parameter, array)
